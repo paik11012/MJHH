4c7c6240f4d4a58cb69ab4942038f7c6be7faccb
gusk94 <h3652k@gmail.com>
2019-11-28 18:16:21 +0900

    addmovie

--- front/src/components/AdminPage.vue
+++ front/src/components/AdminPage.vue
@@ -6,8 +6,27 @@
       <option value=0>전체</option>
       <option v-for="genre in genres" v-bind:key="genre.id" :value="genre.id">{{genre.name}}</option>
     </select>
-    <div class="row mt-5">
-      <button @click="addMovie" class="btn btn-success">추가</button>
+    <br>
+    <div>
+      <b-button v-b-modal.modal-1 class="btn btn-success">추가</b-button>
+      <b-modal id="modal-1" title="Add Movie"
+        :header-bg-variant="headerBgVariant"
+        :header-text-variant="headerTextVariant"
+        :body-bg-variant="bodyBgVariant"
+        :body-text-variant="bodyTextVariant"
+        :footer-bg-variant="footerBgVariant"
+        :footer-text-variant="footerTextVariant"
+      >
+        <!-- <button @click="onModify(movie.id)" class="btn btn-danger" type="submit" id="modi">수정</button> -->
+        제목: <input type="text" v-model="title" id="title"> <br>
+        포스터url: <input type="text" v-model="poster_url" id="poster_url"><br>
+        감독: <input type="text" v-model="director" id="director"><br>
+        배우: <input type="text" v-model="actor" id="actor"><br>
+        개봉일: <input type="text" v-model="open_date" id="open_date"><br>
+        상영시간: <input type="text" v-model="running_time" id="running_time"><br>
+        장르: <input type="text" v-model="genre" id="genre"><br>
+        상세정보: <textarea name="" id="" cols="40" v-model="description" rows="12"></textarea>
+      </b-modal>
     </div>
     <div class="row mt-5">
       <MovieListItem v-for="movie in movieList" :movie="movie" :key="movie.id" />
@@ -27,9 +46,24 @@ export default {
     MovieListItem
   },
   data() {
-
     return {
-      genreid: 0
+      genreid: 0,
+      title: '',
+      director: '',
+      poster_url: '',
+      actor: '',
+      running_time:'',
+      grade: '',
+      description: '',
+      open_date:'',
+      genre:'',
+      variants: ["light", "dark"],
+      headerBgVariant: "dark",
+      headerTextVariant: "light",
+      bodyBgVariant: "dark",
+      bodyTextVariant: "light",
+      footerBgVariant: "dark",
+      footerTextVariant: "dark"
     };
   },
   props: {
